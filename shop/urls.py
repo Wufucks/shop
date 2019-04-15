fcde2383db1b387104be08366192b0afc2d8b331
wujialaoer <624334922@qq.com>
2019-04-15 15:41:35 +0800

    更新支付相关

--- shop/urls.py
+++ shop/urls.py
@@ -22,7 +22,7 @@ from goods.views import *
 from rest_framework.routers import DefaultRouter
 # from rest_framework.authtoken import views
 from rest_framework_jwt.views import obtain_jwt_token
-from trade.views import ShopCartViewSet
+from trade.views import ShopCartViewSet, OrderInfoViewSet,AlipayView
 from shop.settings import MEDIA_ROOT
 from user.views import SmsCodeViewSet, UserViewSet
 
@@ -41,6 +41,8 @@ router.register(r'userfavs', UserViewFavSet, base_name='userfavs')
 router.register(r'leaving', LeavingMessageViewSet, base_name='leaving')
 router.register(r'address', AddressViewSet, base_name='address')
 router.register(r'shopcart', ShopCartViewSet, base_name='shopcart')
+router.register(r'orders', OrderInfoViewSet, base_name='orders')
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
@@ -51,5 +53,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     # url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^login/', obtain_jwt_token),
+    url(r'^alipay/return/', AlipayView.as_view(), name="alipay"),
 
 ]
